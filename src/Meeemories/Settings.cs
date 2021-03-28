e2f6976dbb06e7d9a8f8f0709857bbeb724147e4
iwate <iwatesoftware@live.jp>
2021-03-28 23:11:26 +0900

    remove unused settings

--- src/Meeemories/Settings.cs
+++ src/Meeemories/Settings.cs
@@ -4,9 +4,6 @@
     {
         public string ConnectionString { get; set; }
         public string ContainerName { get; set; }
-        public string TableName { get; set; }
-        public string ImageQueueName { get; set; }
-        public string VideoQueueName { get; set; }
         public string Password { get; set; }
         public int ExpiredMunites { get; set; }
     }
