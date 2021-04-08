094a54279e17eca23fff0d036ccd1cb2767ecac1
iwate <iwatesoftware@live.jp>
2021-04-08 23:40:47 +0900

    add login animation

--- src/Meeemories/ClientApp/src/pages/Login.vue
+++ src/Meeemories/ClientApp/src/pages/Login.vue
@@ -3,7 +3,7 @@
         <img src="/img/mstile-150x150.png"/>
         <p class="error" v-if="message">{{message}}</p>
         <input class="input" type="text" v-model="password" @keyup.enter="trigger" placeholder="合言葉"/>
-        <button class="btn btn-primary" @click="login">ログイン</button>
+        <button :class="btnClass" @click="login">ログイン</button>
     </section>
 </template>
 <script>
@@ -11,29 +11,44 @@
         data() {
             return {
                 password: null,
-                message: null
+                message: null,
+                loading: false,
+            }
+        },
+        computed: {
+            btnClass() {
+                return {
+                    'btn': true,
+                    'btn-primary': true,
+                    'btn-loading': this.loading
+                }
             }
         },
         methods: {
             async login() {
-                const response = await fetch('/api/token', {
-                    method: 'post',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({ password: this.password })
-                })
-                const text = await response.text();
-                if (response.ok) {
-                    const token = JSON.parse(text);
-                    this.message = null;
-                    this.$state.token = token;
-                    localStorage.setItem('token', token);
-                    this.$router.push('/');
-                }
-                else {
-                    this.message = '合言葉が違います。';
-                    console.error(text);
+                this.loading = true;
+                try {
+                    const response = await fetch('/api/token', {
+                        method: 'post',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({ password: this.password })
+                    })
+                    const text = await response.text();
+                    if (response.ok) {
+                        const token = JSON.parse(text);
+                        this.message = null;
+                        this.$state.token = token;
+                        localStorage.setItem('token', token);
+                        this.$router.push('/');
+                    }
+                    else {
+                        this.message = '合言葉が違います。';
+                        console.error(text);
+                    }
+                } finally {
+                    this.loading = false;
                 }
             },
             trigger(evt) {
